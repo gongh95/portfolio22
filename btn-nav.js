54d8046e837f2b6d02ddf61b10d8c6c91dea781b
gongh95 <gonzalo.simon.1995@gmail.com>
2022-03-22 18:37:49 -0300

    finalizado diseño nav

--- btn-nav.js
+++ btn-nav.js
@@ -1,9 +1,17 @@
 const botonNav = document.getElementById("btn-nav-container");
 const contenedorNav = document.querySelector(".ul-container");
+const links = contenedorNav.getElementsByTagName("li");
 
 function cambiarMenuToggle() {
     botonNav.classList.toggle("cerrar");
     contenedorNav.classList.toggle("abierto");
+    // botonNav.addEventListener('click', cambiarMenuToggle);
+}
+function esconderAlApretarLink() {
+    if (botonNav.classList.contains("cerrar") && contenedorNav.classList.contains("abierto")) {
+    botonNav.classList.remove("cerrar");
+    contenedorNav.classList.remove("abierto");
+    }
 }
 
-addEventListener('click', cambiarMenuToggle);
+// addEventListener('click', cambiarMenuToggle);
